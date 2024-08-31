745ab115742e7a50784728aefa2b9088dc33a45c
maho <maho.sakitani@gmail.com>
2024-08-31 18:46:16 +0900

    inital  setup

---
+++ app/Models/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Article extends Model
+{
+
+
+    public function getList() {
+        // articlesテーブルからデータを取得
+        $articles = DB::table('articles')->get();
+
+        return $articles;
+    }
+
+    /*use HasFactory; 編集前*/
+}
